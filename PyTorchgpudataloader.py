7f3f38fb036b0246f7f9c3a1bf85e1daef769320
Skye Hart <smhart@Skyes-MacBook-Pro.local>
2019-12-16 15:45:19 -0500

    ok

--- PyTorchgpudataloader.py
+++ PyTorchgpudataloader.py
@@ -76,11 +76,11 @@ for epoch in range(100):  # loop over the dataset multiple times
 
     running_loss = 0.0
     for i, data in enumerate(trainloader, 0):
-        inputs, labels = data
+        inputs, labels = data[0].to(device), data[1].to(device)
 
         optimizer.zero_grad()
 
-        outputs = net(inputs.float())
+        outputs = net(inputs)
         loss = criterion(outputs, labels)
         loss.backward()
         optimizer.step()
